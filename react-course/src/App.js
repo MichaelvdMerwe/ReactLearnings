1ae907cfea420fe72798b86a5715d47fed89935e
MichaelvdMerwe <vdmerwe.michael2@gmail.com>
2020-04-05 15:21:29 +0200

    update

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -24,23 +24,34 @@ import './App.css';
 // }
 
 class App extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            monsters: [
+                {
+                    id: 'mon1',
+                    name: 'Frankenstein',
+                },
+                {
+                    id: 'mon2',
+                    name: 'Dracula',
+                },
+                {
+                    id: 'mon3',
+                    name: 'Zombie',
+                },
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                {
+                    this.state.monsters.map((monster) => <h1 key={monster.id}>{ monster.name }</h1>)
+                }
             </div>
         )
     }
---
+++ react-course/src/arrays.js
@@ -0,0 +1,10 @@
+// map iterates over an array and executes a function on each of the elemenmts, this will return a new array with the updated values
+map();
+
+filter();
+
+reduce();
+
+find();
+
+includes();
